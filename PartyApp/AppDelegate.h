b21e0749b7f4b51edc801000eca3478c427e8433
Varun <Admin@Channis-MacBook-Pro-2.local>
2014-06-14 13:55:31 +0530

    Initial Commit

---
+++ PartyApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PartyApp
+//
+//  Created by Varun on 14/06/2014.
+//  Copyright (c) 2014 WebFactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
